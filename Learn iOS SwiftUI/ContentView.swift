1398470559cd0521c016b9973115845e6e20f327
alifu <alif.bangets@gmail.com>
2021-04-06 14:32:12 +0700

    initial commit

---
+++ Learn iOS SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Learn iOS SwiftUI
+//
+//  Created by alifu on 06/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Learn iOS SwiftUI/Learn_iOS_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_iOS_SwiftUIApp.swift
+//  Learn iOS SwiftUI
+//
+//  Created by alifu on 06/04/21.
+//
+
+import SwiftUI
+
+@main
+struct Learn_iOS_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
